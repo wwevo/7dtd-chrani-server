c2e4a99c8f694e075474669b6cb42ffb848444d1
Christian Voigt <wwevo@users.noreply.github.com>
2017-12-25 12:28:43 +0100

    classified telnet-stuff further minor changes

--- public/chrani.py
+++ public/chrani.py
@@ -28,6 +28,7 @@ class Player(R.Raba):
 
     id = rf.Primitive()
     name = rf.Primitive()
+    # I'd like to have coordinates as their own class, it's lame to do them one by one (x,y,z)
     pos_x = rf.Primitive()
     pos_y = rf.Primitive()
     pos_z = rf.Primitive()
@@ -63,40 +64,35 @@ class Location(R.Raba):
         pass
 
 
-def setup_telnet_connection(tn):
-    """
-    for now, until i know better, i want each part of this bot using
-    their own telnet. i'm not actually sure if it works that way, but it seems
-    like it ^^
-    """
-    # this is the exact prompt from the games telnet. it might change with a new game-version
-    password_prompt = tn.read_until("Please enter password:")
-    tn.write(PASS.encode('ascii') + b"\r\n")
-    # last 'welcome' line from the games telnet. it might change with a new game-version
-    return tn.read_until("Press 'exit' to end session.")
-
-
 class TelnetCommand:
     command_tn = None
 
     def __init__(self):
         while self.command_tn is None:
-            try:
-                self.command_tn = telnetlib.Telnet(HOST, PORT)
-                self.authenticate(self.command_tn)
-            except Exception, err:
-                time.sleep(4)
+            self.command_tn = TelnetCommand.get_connection()
 
     def __del__(self):
         if self.command_tn: self.command_tn.close()
 
     @staticmethod
-    def authenticate(connection):
+    def get_connection():
         """
         for now, until i know better, i want each part of this bot using
         their own telnet. i'm not actually sure if it works that way, but it seems
         like it ^^
         """
+        try:
+            connection = telnetlib.Telnet(HOST, PORT)
+            TelnetCommand.authenticate(connection)
+            return connection
+        except Exception, err:
+            print "could not connect to the games telnet"
+            time.sleep(4)
+
+        return None
+
+    @staticmethod
+    def authenticate(connection):
         # this is the exact prompt from the games telnet. it might change with a new game-version
         password_prompt = connection.read_until("Please enter password:")
         connection.write(PASS.encode('ascii') + b"\r\n")
@@ -128,120 +124,6 @@ class TelnetCommand:
             if re.match(r"^(.+?) (.+?) INF Chat: \'.*\':.* " + re.escape(message) + "\r", response) is not None:
                 return send_message_response_raw
 
-class PollPlayers(Thread):
-    poll_frequency = 2
-    list_players_tn = None
-    list_players_response_time = 0  # record the runtime of the entire poll
-
-    class PlayerList(Thread):
-        """
-        I've put this in here cause it's never gonna be needed
-        outside of PollPlayers.
-        """
-        list_players_raw = None
-        list_players_array = None
-
-        def __init__(self, event, list_players_raw):
-            Thread.__init__(self)
-            self.stopped = event
-            self.list_players_raw = list_players_raw
-
-        def run(self):
-            self.update(self.list_players_raw)
-            self.stopped.set()
-
-        def update(self, list_players_raw):
-            player_line_regexp = r"\d{1,2}. id=(\d+), ([\w+]+), pos=\((.?\d+.\d), (.?\d+.\d), (.?\d+.\d)\), rot=\((.?\d+.\d), (.?\d+.\d), (.?\d+.\d)\), remote=(\w+), health=(\d+), deaths=(\d+), zombies=(\d+), players=(\d+), score=(\d+), level=(\d+), steamid=(\d+), ip=(\d+\.\d+\.\d+\.\d+), ping=(\d+)\n*"
-            for m in re.finditer(player_line_regexp, list_players_raw):
-                """
-                m.group(16) = steamid
-                """
-                try:
-                    player = Player(steamid = m.group(16))
-                except KeyError:
-                    player = Player()
-
-                player.id = m.group(1)
-                player.name = m.group(2)
-                player.pos_x = m.group(3)
-                player.pos_y = m.group(4)
-                player.pos_z = m.group(5)
-                player.rot_x = m.group(6)
-                player.rot_y = m.group(7)
-                player.rot_z = m.group(8)
-                player.remote = m.group(9)
-                player.health = m.group(10)
-                player.deaths = m.group(11)
-                player.zombies = m.group(12)
-                player.players = m.group(13)
-                player.score = m.group(14)
-                player.level = m.group(15)
-                player.steamid = m.group(16)
-                player.ip = m.group(17)
-                player.ping = m.group(18)
-                player.save()
-
-    def __init__(self, event):
-        while self.list_players_tn is None:
-            try:
-                self.list_players_tn = telnetlib.Telnet(HOST, PORT)
-                TelnetCommand.authenticate(self.list_players_tn)
-            except Exception, err:
-                time.sleep(4)
-
-        Thread.__init__(self)
-        self.stopped = event
-
-    def __del__(self):
-        if self.list_players_tn: self.list_players_tn.close()
-
-    def run(self):
-        """
-        recorded the runtime of the poll, using it to calculate the exact wait
-        time between executions
-        """
-        next_poll = 0;
-        while not self.stopped.wait(next_poll):
-            """
-            basically an endless loop
-            fresh player-data is about the most important thing for this bot :)
-            """
-            next_poll = self.poll_frequency - self.list_players_response_time
-            list_players_raw, player_count = self.poll_players()
-            print "player-data poll is active ({0} players, {1} bytes received, response-time: {2} seconds)".format(str(player_count), str(len(list_players_raw)), str(round(self.list_players_response_time, 3)).ljust(5, '0'))
-
-            # not sure if this is the way to go, but I wanted to have this in it's own thread so the time spend in the
-            # actual server-transaction won't be delayed
-            store_player_list_event = Event()
-            store_player_list_thread = self.PlayerList(store_player_list_event, list_players_raw)
-            store_player_list_thread.start()
-
-    def poll_players(self):
-        """
-        polls live player data from the games telnet
-        times the action to allow for more accurate wait time
-        returns complete telnet output and player count
-        """
-        profile_timestamp_start = time.time()
-
-        list_players_response_raw = ""
-        self.list_players_tn.write("lp" + b"\r\n")
-        while list_players_response_raw == "" or response:
-            """
-            fetches the response of the games telnet 'lp' command
-            (lp = list players)
-            last line from the games lp command, the one we are matching,
-            might change with a new game-version
-            """
-            response = self.list_players_tn.read_until(b"\r\n")
-            list_players_response_raw = list_players_response_raw + response
-
-            m = re.search(r"^Total of (\d{1,2}) in the game\r\n", response)
-            if m:
-                player_count = m.group(1)
-                self.list_players_response_time = time.time() - profile_timestamp_start
-                return list_players_response_raw, player_count
-
 
 class ChatObserverLoop(Thread):
     """
@@ -252,12 +134,7 @@ class ChatObserverLoop(Thread):
 
     def __init__(self, event):
         while self.loop_tn is None:
-            try:
-                self.loop_tn = telnetlib.Telnet(HOST, PORT)
-                TelnetCommand.authenticate(self.loop_tn)
-            except Exception, err:
-                print "could not connect to the games telnet"
-                time.sleep(4)
+            self.loop_tn = TelnetCommand.get_connection()
 
         Thread.__init__(self)
         self.stopped = event
@@ -280,6 +157,7 @@ class ChatObserverLoop(Thread):
 
         tn_cmd = TelnetCommand()
         print "bot is ready and listening"
+        tn_cmd.send_message("Hi there. Command me!")
         timeout_start = None
         execution_time = 0.0
         while not self.stopped.wait(1):
@@ -331,7 +209,7 @@ class ChatObserverLoop(Thread):
 
                         tn_cmd.send_message(player_name + " has set up a lobby. Good job!")
                     else:
-                         tn_cmd.send_message(player_name + " needs to enter the password to get access to sweet commands!")
+                        tn_cmd.send_message(player_name + " needs to enter the password to get access to sweet commands!")
 
                 elif command == "make this my home":
                     if player.authenticated:
